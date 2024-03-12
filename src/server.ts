2a069fc416e2c58eec13f06c89dacf3fe2e53215
antoniocristovam <antoniocristovam@outlook.com.br>
2024-03-12 18:33:14 -0300

    feat: config project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
     host: "0.0.0.0",
-    port: 3000,
+    port: env.PORT,
   })
   .then(() => {
-    console.log("🚀 HTTP Server Running ");
+    console.log(`🚀 HTTP Server Running: localhost:${env.PORT}`);
   });
