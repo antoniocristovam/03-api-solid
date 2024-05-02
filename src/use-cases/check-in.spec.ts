f17e3bb920f45d42d5caf76985abaa99580ae8bb
antoniocristovam <antoniocristovam@outlook.com.br>
2024-05-01 21:05:21 -0300

    test: add check-in

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from "vitest";
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-in-repository";
 import { CheckInUseCase } from "./check-in";
 
@@ -10,14 +10,38 @@ describe("Check-in Use Case", () => {
     beforeEach(() => {
       userRepository = new InMemoryCheckInsRepository();
       sut = new CheckInUseCase(userRepository);
+      vi.useFakeTimers();
     });
   });
 
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
   it("should hash user password upon check in", async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
     });
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should be able to check in twice but in different days.", async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    });
+
     expect(checkIn.id).toEqual(expect.any(String));
   });
 });
