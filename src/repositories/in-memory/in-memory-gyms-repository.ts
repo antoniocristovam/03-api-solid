ccb5f05e4a3d621b828985a12e3a607904cc4963
antoniocristovam <antoniocristovam@outlook.com.br>
2024-07-30 14:55:55 -0300

    feat: Add create method to in-memory gyms repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
 import { GymRepository } from "../gyms-repository";
+import { randomUUID } from "crypto";
 
 export class InMemoryGymsRepository implements GymRepository {
   public items: Gym[] = [];
@@ -8,6 +9,20 @@ export class InMemoryGymsRepository implements GymRepository {
     const gym = this.items.find((item) => item.id === id);
     if (!gym) return null;
 
+    return gym;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: randomUUID(),
+      title: data.title,
+      descrition: data.descrition ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    };
+    this.items.push(gym);
     return gym;
   }
 }
